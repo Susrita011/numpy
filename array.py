5faa2ccd2094465b5495f45250ad2b0eb7804e81
Susrita011 <susritapatnaik11@gmail.com>
2024-09-16 13:19:24 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,60 @@
+# import numpy as np
+# arr = np.array([10,20,30])
+# print(arr)
+
+# import numpy as np
+# arr = np.array([[10,20,30],[40,50,60],[70,80,90]])
+# print(arr)
+
+# import numpy as np
+# arr = np.array([[10,20,30,40],[50,60,70,80],[90,100,110,120]])
+# print(arr[0,0:3])
+# print(arr[1,1:3])
+# print(arr[2,2:])
+
+
+#SORT
+# import numpy as np
+# ar = np.array([[7,6,12,5,3],[10,14,9,7,11]])
+# print(np.sort(ar))
+
+
+#SEARCH and SORT
+# import numpy as np
+# ar = np.array([4,5,3,2])
+# ss = np.where(ar==5)
+# aw = np.where(ar % 2 == 0)
+# print(ss)
+# print(aw)
+
+
+# import numpy as np
+# ar = np.array([1,2,3,4,5])
+# lk = np.searchsorted(ar,4)
+# print(lk)
+#
+
+
+
+#FILTER
+# import numpy as np
+# ar = np.array([20,30,40,50])
+# da = [True,False,True,False]
+# yu =ar[da]
+# print(yu)
+
+#another example
+# import numpy as np
+# ar = np.array([20,30,40,50])
+# fa = ar > 35
+# new = ar[fa]
+# print(new)
+
+
+#another example
+# import numpy as np
+# ar = np.array([2,3,4,5])
+# da = ar % 2 == 0
+# few = ar[da]
+# print(few)
+
---
+++ pandas.py
@@ -0,0 +1,3 @@
+import pandas as pd
+dict = {"Fruits":["Banana","Mango","Apple","Papaya"],
+        "Quantity":[12,10,14,3]}
